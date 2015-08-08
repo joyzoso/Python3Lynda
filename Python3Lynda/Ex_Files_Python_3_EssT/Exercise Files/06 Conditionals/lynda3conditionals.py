ccba2279683cd511e7a4af25cd16eda0cbca1854
joyzoso <joyrockon@gmail.com>
2015-08-08 15:09:58 -0700

    Examples of conditionals

---
+++ Python3Lynda/Ex_Files_Python_3_EssT/Exercise Files/06 Conditionals/lynda3conditionals.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+#def main():
+   # a,b = 0,1
+   # if a < b:
+      #  print('this is true')
+   # else:
+       # print('it is not true')
+
+#def main():
+   # v = ""
+   # if v == 'fun':
+   #     print('v is fun')
+   # elif v == 'dumb':
+   #     print('v is dumb')
+   # elif v == 'none':
+   #     print('v is none')
+   # else:
+    #    print('v is nothing at all')
+
+
+
+def main():
+    a,b = 1,1
+    v = 'this is true' if a < b else 'this is not true' #shortcut expression for the below
+   # if a < b:
+    #    v = 'this is true'
+   # else:
+   #     v = 'this is not true'
+    print(v)
+
+if __name__ == "__main__": main()
