7fe1b41e0c66207b30afe870cfbb8d14912dd47d
joyzoso <joyrockon@gmail.com>
2015-08-10 14:00:29 -0700

    using standard methods for strings with reference

---
+++ Python3Lynda/Ex_Files_Python_3_EssT/Exercise Files/13 Strings/lynda3Standardstringmethods.py
@@ -0,0 +1,7 @@
+s = 'hey hey what can I do'
+new = s.center(80)
+print(new) #string is now 80 characters wide as seen below
+print(len(new))
+
+#note this webpage for more reference on string methods
+#https://docs.python.org/2/library/stdtypes.html#string-methods
