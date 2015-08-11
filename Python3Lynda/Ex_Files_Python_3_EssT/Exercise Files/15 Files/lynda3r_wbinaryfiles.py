1717887b1ed2e431b4405ae09fa4bcef4860a037
joyzoso <joyrockon@gmail.com>
2015-08-10 17:32:41 -0700

    buffered I/O with binary files reading and writing binary files

---
+++ Python3Lynda/Ex_Files_Python_3_EssT/Exercise Files/15 Files/lynda3r_wbinaryfiles.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def main():
+    buffersize = 50000
+    infile = open('olives.jpg', 'rb') #read binary
+    outfile = open('new.jpg', 'wb') #write binary
+    buffer = infile.read(buffersize)
+    while len(buffer):
+        outfile.write(buffer)
+        print('.', end = '')
+        buffer = infile.read(buffersize)
+    print()
+    print('Done.')
+
+
+
+
+if __name__ == "__main__": main()
+
